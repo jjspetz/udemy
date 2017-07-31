a7af7f7ee272b57e4afda2146d8710120a4eec7e
JJ <jj.spetseris@yahoo.com>
2017-07-30 19:48:46 -0500

    more notes added

--- Angular4MasterClass/ts-hello/main.ts
+++ Angular4MasterClass/ts-hello/main.ts
@@ -10,7 +10,35 @@ function doSomething() {
   for(let i=0; i<5; i++) {
     console.log(i);
   }
+  // throws error, but will complie just fine
   console.log('Final:', i);
 }
 
-doSomething();
+// doSomething();
+
+// typescript gives variable type on creation
+let count =15;
+// marks error of wrong type but will compile
+count = 'a';
+// typescript doesn't assign unintiated variable a type
+let a;
+a = 1;
+a = true;
+a = 'a';
+// typescript can be assigned type after :
+let b: number;
+a = 1;
+a = true;
+a = 'a';
+// types include
+// :number
+// :boolean
+// :string
+// :any
+// :number[]
+// :any[]
+// enum
+
+// example declaration of enum
+enum Color {Red, Green, Blue};
+let backgroundColor = Color.Red;
