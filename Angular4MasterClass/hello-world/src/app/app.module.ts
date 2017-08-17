b67eb0728f6cd3b3ec4e2578a14e1840513293ec
JJ <jj.spetseris@yahoo.com>
2017-08-17 14:10:08 -0500

    lesson 31

--- Angular4MasterClass/hello-world/src/app/app.module.ts
+++ Angular4MasterClass/hello-world/src/app/app.module.ts
@@ -2,12 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { CoursesComponent } from './courses.component'
+import { CoursesComponent } from './courses.component';
+import { CourseComponent } from './course/course.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    CoursesComponent
+    CoursesComponent,
+    CourseComponent
   ],
   imports: [
     BrowserModule
