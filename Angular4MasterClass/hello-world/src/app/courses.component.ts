cffc5534d81f2bdadc61962d066aaa968721b49b
JJ <jj.spetseris@yahoo.com>
2017-08-17 13:48:40 -0500

    lesson 30

---
+++ Angular4MasterClass/hello-world/src/app/courses.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'courses', // uses css selectors, also supports extra html tag
+  template: '<h2>Courses</h2>'
+})
+export class CoursesComponent {
+
+}
