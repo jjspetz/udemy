c499b560b9582f5bdacdd6dba590ca0ec46b5ba0
JJ <jj.spetseris@yahoo.com>
2017-08-17 14:12:27 -0500

    lesson 32

--- Angular4MasterClass/hello-world/src/app/courses.component.ts
+++ Angular4MasterClass/hello-world/src/app/courses.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'courses', // uses css selectors, also supports extra html tag
-  template: '<h2>Courses</h2>'
+  template: '<h2>{{title}}</h2>' // brackets except valid JS as well
 })
 export class CoursesComponent {
-
+  title = 'List of Courses';
 }
