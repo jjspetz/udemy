b560ea2e126aa411906fdc71bb4e40e77354f08c
JJ <jj.spetseris@yahoo.com>
2017-08-24 16:10:08 -0500

    lesson 34

--- Angular4MasterClass/hello-world/src/app/courses.component.ts
+++ Angular4MasterClass/hello-world/src/app/courses.component.ts
@@ -1,15 +1,20 @@
 import { Component } from '@angular/core';
+import { CourseService } from './course.service'
 
 @Component({
   selector: 'courses', // uses css selectors, also supports extra html tag
   template: `
     <h2>{{title}}</h2>
     <ul>
-      <li *ngFor='let course of courses'> {{course}} </li>
+      <li *ngFor='let course of courses'> {{this.course}} </li>
     </ul>
     `
 })
 export class CoursesComponent {
   title = 'List of Courses';
-  courses = ['course 1', 'course 2', 'course 3'];
+  courses;
+
+  constructor(service: CourseService) {
+    this.courses = service.getCourses();
+  }
 }
