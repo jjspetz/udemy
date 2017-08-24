af6a48343a0def0dc12a10100fe0094b8bcac452
JJ <jj.spetseris@yahoo.com>
2017-08-24 15:35:38 -0500

    lesson 33

--- Angular4MasterClass/hello-world/src/app/courses.component.ts
+++ Angular4MasterClass/hello-world/src/app/courses.component.ts
@@ -2,8 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'courses', // uses css selectors, also supports extra html tag
-  template: '<h2>{{title}}</h2>' // brackets except valid JS as well
+  template: `
+    <h2>{{title}}</h2>
+    <ul>
+      <li *ngFor='let course of courses'> {{course}} </li>
+    </ul>
+    `
 })
 export class CoursesComponent {
   title = 'List of Courses';
+  courses = ['course 1', 'course 2', 'course 3'];
 }
