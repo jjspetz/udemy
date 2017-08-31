ca420a168c143adc92c0e6555995c081543b172b
JJ <jj.spetseris@yahoo.com>
2017-08-31 10:25:49 -0500

    NumberWizard refactored

--- Unity/NumberWizard.cs
+++ Unity/NumberWizard.cs
@@ -3,14 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class NumberWizard : MonoBehaviour {
-	
+
 	// global variables
-	int max = 1000;
-	int min = 1;
-	int guess = 500;
+	int max;
+	int min;
+	int guess;
 
 	// Use this for initialization
 	void Start () {
+		GameInitialize ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.UpArrow)) {
+			min = guess;
+			NextGuess();
+		}
+		else if (Input.GetKeyDown(KeyCode.DownArrow)) {
+			max = guess;
+			NextGuess();
+		}
+		else if (Input.GetKeyDown(KeyCode.Return)) {
+			print ("I got it.");
+			GameInitialize ();
+		}
+	}
+
+	void GameInitialize() {
+		// reinitilize variables
+		min = 0;
+		max = 1001;
+		guess = 500;
+
+		print ("++++++++++++++++++++++++++++");
 		print ("Welcome to Number Wizard.");
 		print ("Pick a number, but keep it secret.");
 
@@ -18,25 +44,12 @@ public class NumberWizard : MonoBehaviour {
 		print ("The lowest number you can pick is " + min);
 
 		print ("Is your number " + guess + "?");
-		print ("up arrow for higher, down arrow for lower, 
-return for equal.");
-
+		print ("up arrow for higher, down arrow for lower, return for equal.");
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (Input.GetKeyDown(KeyCode.UpArrow)) {
-			min = guess;
-			guess = (max + min) / 2;
-			print ("Higher or lower than " + guess);
-		}
-		else if (Input.GetKeyDown(KeyCode.DownArrow)) {
-			max = guess;
-			guess = (max + min) / 2;
-			print ("Higher or lower than " + guess);
-		}
-		else if (Input.GetKeyDown(KeyCode.Return)) {
-			print ("I got it.");
-		}
+
+	void NextGuess() {
+		guess = (max + min) / 2;
+		print ("Higher or lower than " + guess);
+		print ("up arrow for higher, down arrow for lower, return for equal.");
 	}
 }
