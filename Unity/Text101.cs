003add6fe73899de6afbc21687f6b28b113c1add
JJ <jj.spetseris@yahoo.com>
2017-10-05 14:19:51 -0500

    update on text101

--- Unity/Text101.cs
+++ Unity/Text101.cs
@@ -6,16 +6,61 @@ using UnityEngine.UI;
 public class TextController : MonoBehaviour {
 
 	public Text text;
+	private enum States {cell, mirror, sheets, locks};
+	private States myState;
+	private string inventory = "";
+//	List<string> inventory = new List<string>();
 
 	// Use this for initialization
 	void Start () {
-		text.text = "Hello World";
+		myState = States.cell;
 	}
 
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown (KeyCode.Space)) {
-			text.text = "You pressed the space bar.";
+		print (myState);
+		if (myState == States.cell) {
+			cell ();
+		} else if (myState == States.sheets) {
+			sheets ();
+		}
+	}
+
+	void cell() {
+		text.text = "You are in a cell. There is a lock on the door, A mirror on the wall, " +
+		"and dirty sheets on the bed.\n\n" +
+		"Press S to view sheets, M to view mirror, or L to view the lock.";
+
+		if (inventory == "sheets") {
+			text.text = "You are in a cell. There is a lock on the door, A mirror on the wall, " +
+				"and dirty sheets in your hand.\n\n" +
+				"Press M to view mirror, or L to view the lock.";
+		} else if (inventory == "mirror") {
+			text.text = "You are in a cell. There is a lock on the door, A mirror in your hand, " +
+				"and dirty sheets on the bed.\n\n" +
+				"Press S to view sheets or L to view the lock.";
+		}
+
+		if (Input.GetKeyDown (KeyCode.S)) {
+			myState = States.sheets;
+		}
+	}
+
+	void sheets() {
+		text.text = "There are dirty sheets on your bed. \n\n";
+
+		if (inventory == "") {
+			text.text += "Press T to take the sheets. ";
+		} else {
+			text.text += "Press T to take the sheets, and drop the " + inventory + ". ";
+		}
+		text.text += "Press R to return.";
+
+		if (Input.GetKeyDown (KeyCode.R)) {
+			myState = States.cell;
+		} else if (Input.GetKeyDown (KeyCode.T)) {
+			inventory = "sheets";
+			myState = States.cell;
 		}
 	}
 }
