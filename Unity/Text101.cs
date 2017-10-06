cbce578cfabdb1765e9e9a600402167e7d8b46dd
JJ <jj.spetseris@yahoo.com>
2017-10-06 09:17:58 -0500

    Text101 finished

--- Unity/Text101.cs
+++ Unity/Text101.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 public class TextController : MonoBehaviour {
 
 	public Text text;
-	private enum States {cell, mirror, sheets, locks};
+	private enum States {cell, mirror, sheets, locks, freedom};
 	private States myState;
 	private string inventory = "";
 //	List<string> inventory = new List<string>();
@@ -23,26 +23,37 @@ public class TextController : MonoBehaviour {
 			cell ();
 		} else if (myState == States.sheets) {
 			sheets ();
+		} else if (myState == States.mirror) {
+			mirror ();
+		} else if (myState == States.locks) {
+			locks ();
+		} else if (myState == States.freedom) {
+			freedom ();
 		}
 	}
 
 	void cell() {
-		text.text = "You are in a cell. There is a lock on the door, A mirror on the wall, " +
-		"and dirty sheets on the bed.\n\n" +
-		"Press S to view sheets, M to view mirror, or L to view the lock.";
 
 		if (inventory == "sheets") {
 			text.text = "You are in a cell. There is a lock on the door, A mirror on the wall, " +
-				"and dirty sheets in your hand.\n\n" +
-				"Press M to view mirror, or L to view the lock.";
+			"and dirty sheets in your hand.\n\n" +
+			"Press M to view mirror, or L to view the lock.";
 		} else if (inventory == "mirror") {
 			text.text = "You are in a cell. There is a lock on the door, A mirror in your hand, " +
+			"and dirty sheets on the bed.\n\n" +
+			"Press S to view sheets or L to view the lock.";
+		} else {
+			text.text = "You are in a cell. There is a lock on the door, A mirror on the wall, " +
 				"and dirty sheets on the bed.\n\n" +
-				"Press S to view sheets or L to view the lock.";
+				"Press S to view sheets, M to view mirror, or L to view the lock.";
 		}
 
-		if (Input.GetKeyDown (KeyCode.S)) {
+		if (Input.GetKeyDown (KeyCode.S) && inventory != "sheets") {
 			myState = States.sheets;
+		} else if (Input.GetKeyDown (KeyCode.M) && inventory != "mirror") {
+			myState = States.mirror;
+		} else if (Input.GetKeyDown (KeyCode.L)) {
+			myState = States.locks;
 		}
 	}
 
@@ -62,5 +73,48 @@ public class TextController : MonoBehaviour {
 			inventory = "sheets";
 			myState = States.cell;
 		}
+	}
+
+	void mirror() {
+		text.text = "The mirror is cracked. You don't look good. \n\n";
+
+		if (inventory == "") {
+			text.text += "Press T to take the mirror. ";
+		} else {
+			text.text += "Press T to take the mirror, and drop the " + inventory + ". ";
+		}
+		text.text += "Press R to return.";
+
+		if (Input.GetKeyDown (KeyCode.R)) {
+			myState = States.cell;
+		} else if (Input.GetKeyDown (KeyCode.T)) {
+			inventory = "mirror";
+			myState = States.cell;
+		}
+	}
+
+	void locks() {
+
+		if (inventory == "sheets") {
+			text.text = "The sheets don't seem to help you open the lock.\n\n" +
+				"Press R to return.";
+		} else if (inventory == "mirror") {
+			text.text = "You use the mirror to see the buttons. Greasy finger prints give away the code.\n\n" +
+				"Press R to return or O to open the door.";
+		} else {
+			text.text = "The lock is a push button lock. If only you could see the buttons\n\n" +
+				"Press R to return.";
+		}
+
+		if (Input.GetKeyDown (KeyCode.R)) {
+			myState = States.cell;
+		} else if (Input.GetKeyDown (KeyCode.O) && inventory == "mirror") {
+			myState = States.freedom;
+		}
+	}
+
+	void freedom() {
+		text.text = "Congratulations, You've gained your freedom.\n\n" +
+					"Game Over";
 	}
 }
